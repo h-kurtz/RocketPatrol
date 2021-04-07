e7bdc2a8f71dabe95341936368fd61898b5aaa33
h-kurtz <hckurtz@ucsc.edu>
2021-04-07 16:58:14 -0700

    rocket and UI implemented!

---
+++ src/scenes/Play.js
@@ -0,0 +1,32 @@
+class Play extends Phaser.Scene{
+    constructor() {
+        super("playScene");
+    }
+
+    preload() {
+        // load images and sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create() {
+    	// place starfield
+    	this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0, 0);
+
+        // green border
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+
+        // white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+    
+        // instantiate player class
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);  
+    }
+    update() {
+        this.starfield.tilePositionX -= starSpeed;
+    }
+}
